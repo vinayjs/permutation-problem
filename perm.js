bf48ad6eb046ea8fc926b66d1d1ec92ed2c6e12d
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-11-23 12:45:07 +0530

    add README.md

---
+++ perm.js
@@ -0,0 +1,16 @@
+// Given a string and a pattern , find out if the string contains any permutation of the pattern.
+
+// permutation is defined as the re-arranging of the characters of the string. 
+// example: "abc" has the following six permutations:
+// 1. abc
+// 2. acb
+// 3. bac
+// 4. bca
+// 5. cab
+// 6. cba
+
+// *  if a string has  'n' distinct characters, it will have n! permutations.
+
+// Input: String="oidbcaf", Pattern="abc"
+// Output: true
+// Explanation: The string contains "bca" which is a permutation of the given pattern.
